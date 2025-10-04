e8534adf64fba57392dff45ad1d4d451a06d0f1f
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2025-10-04 10:27:00 -0500

    crecion de servicios (en proceso)

---
+++ lib/services/advertisement_service.dart
@@ -0,0 +1,9 @@
+import 'package:swallow_app/models/advertisement.dart';
+
+class AdvertisementService {
+  static const String baseUrl = "http://localhost:3210/anuncios";
+
+  static Future<Advertisement> create(int idVacante, http) async {
+    
+  }
+}
